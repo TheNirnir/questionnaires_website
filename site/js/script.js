afb8df38bf05d53f7e8ac64b2d6867cba743f1c6
TheNirnir <nirnirba@gmail.com>
2020-09-07 14:55:59 +0300

    m

---
+++ site/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+	// document.getElementById('button').style.height = document.getElementById('button').offsetWidth + "px";
+	// // document.getElementById('button').style.height = 200;
+	// console.log(document.getElementById('button').offsetWidth);
+	// console.log(document.getElementById('button').offsetHeight);
+	// console.log(document.getElementById('button').style.height);
+
+	document.getElementById("main-content").style.paddingBottom = document.getElementById("footer").offsetHeight + 30 + "px";
+	// console.log(document.getElementById("footer").offsetHeight + "px");
+	// console.log(document.getElementById("main-content").style.paddingBottom);
+});
