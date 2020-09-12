e52a2ee7772103ca29ecbef223a9f081b9df8572
TheNirnir <nirnirba@gmail.com>
2020-09-12 17:09:05 +0300

    m

--- site/js/script.js
+++ site/js/script.js
@@ -6,9 +6,25 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	// console.log(document.getElementById('button').style.height);
 
 	document.getElementById("main-content").style.marginTop = document.getElementsByTagName("header")[0].offsetHeight + 20 + "px";
-	document.getElementById("main-content").style.paddingBottom = document.getElementById("footer").offsetHeight + 20 + "px";
+	document.getElementById("main-content").style.paddingBottom = document.getElementById("footer").offsetHeight + 30 + "px";
 	// console.log(document.getElementById("footer").offsetHeight + "px");
 	// console.log(document.getElementById("main-content").style.paddingBottom);
-	console.log(document.getElementsByTagName("header")[0].offsetHeight + 30 + "px");
-	console.log(document.getElementById("main-content").style.marginTop);
+	// console.log(document.getElementsByTagName("header")[0].offsetHeight + 30 + "px");
+	// console.log(document.getElementById("main-content").style.marginTop);
+
+	if (screen.width > 768) {
+		document.getElementById("button-2").style.height = document.getElementById("button-3").offsetHeight + "px";
+		document.getElementById("button-4").style.height = document.getElementById("button-3").offsetHeight + "px";
+
+		document.getElementById("button-5").style.height = document.getElementById("button-6").offsetHeight + "px";
+		document.getElementById("button-7").style.height = document.getElementById("button-6").offsetHeight + "px";
+	}
+	else {
+		document.getElementById("button-2").style.height = document.getElementById("button-3").offsetHeight + "px";
+		document.getElementById("button-5").style.height = document.getElementById("button-4").offsetHeight + "px";
+		document.getElementById("button-7").style.height = document.getElementById("button-6").offsetHeight + "px";
+	}
+
+	// console.log(document.getElementById("button-3").offsetHeight + "px");
+	// console.log(screen.width);
 });
