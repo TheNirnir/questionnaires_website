de0620814e81a98360f684557a3965825f183569
TheNirnir <nirnirba@gmail.com>
2021-04-24 21:42:23 +0300

    m

---
+++ site/data/MDSI-data.js
@@ -0,0 +1,22 @@
+var MDSIObj = {
+	shortName: "MDSI",
+	fullName: "Multydimensional Driving Style Inventory",
+	about: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.",
+	languages: [
+		{
+			country: "israel",
+			language: "Hebrew"
+		},
+		{
+			country: "usa",
+			language: "English"
+		}
+	],
+	publications: [
+		"Taubman – Ben‑Ari, O., Mikulincer, M., & Gillath, O. (2004). The Multidimensional Driving Style Inventory – Scale construct and validation. Accident Analysis and Prevention, 36, 323‑332.",
+
+		"Taubman – Ben‑Ari, O., Mikulincer, M., & Gillath, O. (2005). From parents to children ‑ Intergenerational transmission of driving styles. Transportation Research Part F: Traffic Psychology and Behaviour, 8, 19‑29.",
+
+		"Taubman – Ben‑Ari, O., & Noy, A. (2011). Does the transition to parenthood influence driving? Accident Analysis and Prevention, 43, 1022‑1035."
+	]
+};
--- site/js/script.js
+++ site/js/script.js
@@ -1,53 +1,103 @@
+var QuestionnaireAboutHtml = "snippets/first_about_snippet.html";
+var QuestionnaireLanguageHtml = "snippets/language_snippet.html";
+var QuestionnaireThirdHtml = "snippets/third_snippet.html";
+
 document.addEventListener("DOMContentLoaded", sizesSet);
+
+// document.addEventListener("DOMContentLoaded", questionnairePage("x"));
 	
-	// document.getElementById('button').style.height = document.getElementById('button').offsetWidth + "px";
-	// // document.getElementById('button').style.height = 200;
-	// console.log(document.getElementById('button').offsetWidth);
-	// console.log(document.getElementById('button').offsetHeight);
-	// console.log(document.getElementById('button').style.height);
-
-	// document.getElementById("main-content").style.marginTop = document.getElementsByTagName("header")[0].offsetHeight + 20 + "px";
-	// document.getElementById("main-content").style.paddingBottom = document.getElementById("footer").offsetHeight + 30 + "px";
-	// console.log(document.getElementById("footer").offsetHeight + "px");
-	// console.log(document.getElementById("main-content").style.paddingBottom);
-	// console.log(document.getElementsByTagName("header")[0].offsetHeight + 30 + "px");
-	// console.log(document.getElementById("main-content").style.marginTop);
-
-// 	if (screen.width > 768) {
-// 		document.getElementById("button-2").style.height = document.getElementById("button-3").offsetHeight + "px";
-// 		document.getElementById("button-4").style.height = document.getElementById("button-3").offsetHeight + "px";
-
-// 		document.getElementById("button-5").style.height = document.getElementById("button-6").offsetHeight + "px";
-// 		document.getElementById("button-7").style.height = document.getElementById("button-6").offsetHeight + "px";
-// 	}
-// 	else {
-// 		document.getElementById("button-2").style.height = document.getElementById("button-3").offsetHeight + "px";
-// 		document.getElementById("button-5").style.height = document.getElementById("button-4").offsetHeight + "px";
-// 		document.getElementById("button-7").style.height = document.getElementById("button-6").offsetHeight + "px";
-// 	}
-
-// 	// for (var i = 0; i < document.getElementsByClassName("no-initials").length; i++) {
-// 	// 	document.getElementsByClassName("no-initials")[i].style.lineHeight = document.getElementsByClassName("no-initials")[i].offsetHeight + "px";
-// 	// }
-// 	document.getElementsByClassName("no-initials")[1].style.lineHeight = document.getElementsByClassName("no-initials")[1].offsetHeight + "px";
-
-// 	// console.log(document.getElementsByClassName("no-initials")[1].offsetHeight + "px");
-// 	// console.log(screen.width);
-
-// document.getElementsByTagName('html').onresize = function (event) {
-// 	document.getElementById('header-trpezoid').style.borderLeftWidth = document.getElementById('header-trpezoid').offsetWidth + "px";
-// 	document.getElementById('footer-trpezoid').style.borderRightWidth = document.getElementById('footer-trpezoid').offsetWidth + "px";
-// };
 window.addEventListener("resize", sizesSet);
 
 function sizesSet(event) {
+	document.getElementById("footer").style.marginTop = 0;
 	document.getElementById('header-trpezoid').style.borderLeftWidth = document.getElementById('header-container').offsetWidth + "px";
 	document.getElementById('footer-trpezoid').style.borderRightWidth = document.getElementById('footer-container').offsetWidth + "px";
 	for (var i = 0; i < document.querySelectorAll('.language-flag').length; i++) {
 		document.getElementsByClassName('language-flag')[i].style.height = document.getElementsByClassName('language-name')[i].offsetHeight + "px";
 	}
+	// console.log(document.getElementsByTagName("html")[0].offsetHeight);
+	// console.log(screen.height);
+
+	// if (screen.height > document.getElementsByTagName("html")[0].offsetHeight) {
+	// 	document.getElementById("footer").style.marginTop = (screen.height - document.getElementsByTagName("html")[0].offsetHeight) + "px";
+	// }
 }
 
-function questionnairePage(questionnaire) {
-	window.location.href = "questionnairesPage.html"
+// console.log(QuestionnaireObj.about);
+// questionnairePage("MDSI");
+// var questionnaire;
+function questionnairePage(questionnaireName) {
+	sessionStorage.setItem("questionnaire", questionnaireName);
+	window.location.href = "questionnairesPage.html";
+	// console.log(questionnaireName);
+	// console.log(questionnaire);
+	// questionnaire = questionnaireName;
+	// setTimeout(buildAndShowQuestionnaire(QuestionnaireObj), 3000);
+	// buildAndShowQuestionnaire(QuestionnaireObj);
+}
+
+function questionnairePageBuild() {
+	// console.log(sessionStorage.getItem("questionnaire"));
+	if (sessionStorage.getItem("questionnaire") == "MDSI") {
+		buildAndShowQuestionnaire(MDSIObj);
+	}
+
+}
+
+function setTitle(QuestionnaireObj) {
+	document.getElementsByTagName("html")[0].innerHTML = insertProperty(document.getElementsByTagName("html")[0].innerHTML, "shortName", QuestionnaireObj.shortName);
+	document.getElementsByTagName("html")[0].innerHTML = insertProperty(document.getElementsByTagName("html")[0].innerHTML, "fullName", QuestionnaireObj.fullName);
+}
+
+function insertProperty (string, propName, propValue) {
+	var propToReplace = "{{" + propName + "}}";
+	string = string.replace(RegExp(propToReplace, "g"), propValue);
+	return string;
+}
+
+function buildAndShowQuestionnaire(QuestionnaireObj) {
+	setTitle(QuestionnaireObj);
+	$ajaxUtils.sendGetRequest(QuestionnaireAboutHtml, function(QuestionnaireAboutHtml) {
+		$ajaxUtils.sendGetRequest(QuestionnaireLanguageHtml, function(QuestionnaireLanguageHtml) {
+			$ajaxUtils.sendGetRequest(QuestionnaireThirdHtml, function(QuestionnaireThirdHtml) {
+				var QuestionnaireViewHTML = buildQuestionnaireViewHTML(QuestionnaireObj, QuestionnaireAboutHtml, QuestionnaireLanguageHtml, QuestionnaireThirdHtml);
+				document.querySelector("#main-content").innerHTML = QuestionnaireViewHTML;
+				sizesSet();
+			}, false);
+		}, false);
+	}, false);
+}
+
+function buildQuestionnaireViewHTML(QuestionnaireObj, QuestionnaireAboutHtml, QuestionnaireLanguageHtml, QuestionnaireThirdHtml) {
+	var finalHtml = QuestionnaireAboutHtml;
+
+	var about = QuestionnaireObj.about;
+
+	finalHtml = insertProperty(finalHtml, "about", about);
+
+	// console.log(QuestionnaireObj.languages[0].country);
+	// console.log(QuestionnaireObj.languages[0].language);
+
+	for (var i = 0; i < QuestionnaireObj.languages.length; i++) {
+		var html = QuestionnaireLanguageHtml;
+		var country = QuestionnaireObj.languages[i].country;
+		var language = QuestionnaireObj.languages[i].language;
+
+		html = insertProperty(html, "country", country);
+		html = insertProperty(html, "language", language);
+
+		finalHtml += html;
+	}
+
+	finalHtml +=  QuestionnaireThirdHtml;
+
+	for (var i = 0; i < QuestionnaireObj.publications.length; i++) {
+		finalHtml += "<li>";
+		finalHtml += QuestionnaireObj.publications[i];
+		finalHtml += "</li>";
+	}
+
+	finalHtml += "</ul></div>";
+
+	return finalHtml;
 }
