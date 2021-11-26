ffeddce72de004ec1a6fd1003a5b8fd385cbdc6b
TheNirnir <nirnirba@gmail.com>
2021-11-26 20:09:52 +0200

    m

--- site/data/DSEQ-data.js
+++ site/data/DSEQ-data.js
@@ -13,7 +13,7 @@ var DSEQObj = {
 		}
 	],
 	publications: [
-		"Eherenfreund-Hager, A., & Taubman – Ben-Ari, O. (2016). The effect of affect induction and personal variables on young drivers’ willingness to drive recklessly. Transportation Research Part F: Traffic Psychology and Behaviour, 41, Part F, 138-149. https://doi.org/10.1016/j.trf.2016.06.008,"
+		"Eherenfreund-Hager, A., & Taubman – Ben-Ari, O. (2016). The effect of affect induction and personal variables on young drivers’ willingness to drive recklessly. Transportation Research Part F: Traffic Psychology and Behaviour, 41, Part F, 138-149. https://doi.org/10.1016/j.trf.2016.06.008,",
 		"Miller, G., & Taubman – Ben-Ari, O. (2010). Driving styles among young novice drivers - The contribution of parental driving styles and personal characteristics. Accident Analysis and Prevention, 42(2), 558-570. https://doi.org/10.1016/j.aap.2009.09.024",
 		"Taubman - Ben-Ari, O. (2008). Motivational sources of driving and their associations with reckless driving cognitions and behavior. European Review of Applied Psychology, 58(1), 51-64. https://doi.org/10.1016/j.erap.2005.08.002",
 		"Taubman – Ben-Ari, O. (2010). Young drivers’ attitudes toward accompanied driving: A new multidimensional measure. Accident Analysis & Prevention, 42(4), 1009-1017. https://doi.org/10.1016/j.aap.2009.12.003"
